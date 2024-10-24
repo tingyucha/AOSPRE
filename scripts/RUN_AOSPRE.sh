5485ba7f0b7b9c3bf57dc574615a090ac813ecd0
bklotz-ncar <84942394+bklotz-ncar@users.noreply.github.com>
2024-10-23 21:58:35 -0600

    Update and rename RUN_AOS.sh to RUN_AOSPRE.sh
    Updated all references from AOS to AOSPRE

--- scripts/RUN_AOSPRE.sh
+++ scripts/RUN_AOSPRE.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# RUN_AOS.sh
+# RUN_AOSPRE.sh
 #
 # Brad Klotz, NCAR Project Scientist
 # Created: 06/16/2021
 
-# This program  will initiate the APAR Observing Simulation (AOS)
-# system code. You will need to specify a few items in order to complete the
+# This program  will initiate the APAR Observing Simulation,
+# Processing, and Research Environment (AOSPRE) system code. 
+# You will need to specify a few items in order to complete the
 # code requirements. You will need to configure your namelist
 # file prior to running this script for now. That functionality
 # may change in the future.
@@ -17,17 +18,17 @@
 # test directory, and output directory.
 
 # Recommended script execution - creates a log file and runs in the background
-# ./RUN_AOS.sh > out_aos.log &
+# ./RUN_AOSPRE.sh > out_aospre.log &
 
-# Base Directory: This should be your main AOS directory you've created
-#                 example: /export/wind1/bradklotz/git/APAR-Observing-Simulator
+# Base Directory: This should be your main AOSPRE directory you've created
+#                 example: /export/wind1/bradklotz/git/AOSPRE
 #
 # Operating Directory: This is the directory containing your namelist and config files
-#                 example: /export/wind1/bradklotz/AOS/tests/WRF_supercell_100m
+#                 example: /export/wind1/bradklotz/AOSPRE/tests/WRF_supercell_100m
 #
 # Scan Mode: This is simply the type of scan - surveillance or rhi
 #
-# Namelist file: This is the file that controls the functionality of the AOS code
+# Namelist file: This is the file that controls the functionality of the AOSPRE code
 #                 example: namelist.surveillance
 #
 # Experiment Directory: If you are performing several runs as part of a larger
@@ -37,7 +38,7 @@
 #
 # Test Directory: This is the name of the test-specific directory you
 #                 want to create. It will typically have information about the
-#		  particular AOS run you are executing.
+#		  particular AOSPRE run you are executing.
 #		  example: supercell_100m_10s_Cband_1kmAGL
 #
 # Output Directory: This is the combined strings of the Base Directory, the
@@ -48,13 +49,13 @@
 ##### USER-DEFINED VARIABLES #####
 
 # Base directory
-base_dir=/export/wind1/bradklotz/APAR-Observing-Simulator
+base_dir=/export/wind1/bradklotz/AOSPRE
 
 # Operating directory
-operating_dir=/export/wind1/bradklotz/AOS/run_tests/WRF_supercell_100m
+operating_dir=/export/wind1/bradklotz/AOSPRE/run_tests/WRF_supercell_100m
 
 # Output base directory
-output_base_dir=/export/wind1/bradklotz/AOS
+output_base_dir=/export/wind1/bradklotz/AOSPRE
 
 # Scan mode: rhi or surveillance
 scan_mode="surveillance"
@@ -68,7 +69,7 @@ experiment_dir="output_tests"
 
 # Test directory
 # This will typically provide some specific information
-# about your run of the AOS - such as the storm type, resolution
+# about your run of the AOSPRE - such as the storm type, resolution
 # aircraft initial height, microwave band, etc.
 test_dir="supercell_100m_10s_1kmAGL_Cband_2.0BW_x215_y315_4490-5090s_FVEL"
 
@@ -92,7 +93,7 @@ fi
 # Get your current working directory
 current_dir=$(pwd)
 
-echo "Running the AOS code..."
+echo "Running the AOSPRE code..."
 echo "Your current working directory is: " ${current_dir}
 echo "Your operating directory is: " ${operating_dir}
 echo "Your scan mode is: " ${scan_mode}
@@ -101,7 +102,7 @@ echo "Your output directory is: " ${output_dir}
 
 ##### END USER VARIABLES #####
 
-##### EXECUTION OF AOS CODE #####
+##### EXECUTION OF AOSPRE CODE #####
 
 # Verifying the base and operating directories  exist
 if [[ ! -d ${base_dir} || -z "$base_dir" ]]; then
@@ -118,7 +119,7 @@ fi
 
 # Create the output directory if it doesn't exist
 echo ""
-echo "All specified directories are verified...Proceeding with AOS execution..."
+echo "All specified directories are verified...Proceeding with AOSPRE execution..."
 
 if [ ! -d ${output_dir} ]; then
 	   mkdir ${output_dir}
@@ -131,19 +132,19 @@ cd ${operating_dir}
 
 # Execute the code
 echo ""
-echo "Initiating AOS code..."
+echo "Initiating AOSPRE code..."
 echo ""
 echo "And off we go..."
 echo ""
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-echo "%%        AOS SPECIFIC OUTPUT BEGINS        %%"
+echo "%%      AOSPRE SPECIFIC OUTPUT BEGINS       %%"
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 
 ./${base_dir}/code/embed-crsim/a.out ${namelist_file}
 
 echo ""
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-echo "%%         AOS SPECIFIC OUTPUT ENDS         %%"
+echo "%%       AOSPRE SPECIFIC OUTPUT ENDS        %%"
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 
 # Once complete, move created files to output directory
@@ -179,6 +180,6 @@ echo "Returning to your original working directory: " ${current_dir}
 cd ${current_dir}
 
 echo ""
-echo "AOS code execution is complete...Done!"
+echo "AOSPRE code execution is complete...Done!"
 
 
